2c0c0cd8d83d6a6dd51b8235ac328c36868beeef
Taras Lahotskyy <taras_80@ukr.net>
2015-09-04 19:21:53 +0000

    Ready block TODAY

---
+++ scripts/weatherByCity.js
@@ -0,0 +1,11 @@
+// JavaScript File
+
+function getWeatherByCity(lang, fnOK, fnError, cityName) {
+    $.getJSON(
+        'http://api.openweathermap.org/data/2.5/forecast/daily?q=' 
+        + cityName + '&cnt=7&units=metric' + '&lang=' + lang + '&callback=?',
+        function (data) {
+            fnOK.call(this, data);
+        }
+    );
+}
