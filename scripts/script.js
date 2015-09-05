770866a9eb2b7610f4494f602ff8a7bd3f1c99f4
Taras Lahotskyy <taras_80@ukr.net>
2015-09-05 10:00:05 +0000

    not js

--- scripts/script.js
+++ scripts/script.js
@@ -2,16 +2,17 @@
 
 $(function(){
     $('#btnGetWeather').click(function () {
-        getWeatherByCity('ua', dataReceived, showError, $('#inputCityName').val());
+        getWeatherByCity('en', dataReceived, showError, $('#inputCityName').val());
     });
     
-    getWeatherData('ua', dataReceived, showError);
+    getWeatherData('en', dataReceived, showError);
     
 
     function dataReceived(data) {
         var offset = (new Date()).getTimezoneOffset()*60*1000; // Відхилення від UTC  в мілісекундах
         var city = data.city.name;
         var country = data.city.country;
+       
         $("#weatherTable tr:not(:first)").remove();
 
         $.each(data.list, function(){
@@ -21,37 +22,51 @@ $(function(){
                 this.weather[0].icon,
                 moment(localTime).calendar(),	// Використовуємо moment.js для представлення дати
                 this.weather[0].description,
+                Math.round(this.temp.day) + '&deg;C',
+                this.humidity + '%',
+                Math.round(this.speed) + 'm/s'
+            );
+        });
+      
+         $.each(data.list, function(){
+            // "this" тримає об'єкт прогнозу звідси: http://openweathermap.org/forecast16
+            var localTime = new Date(this.dt*1000 - offset); // конвертуємо час з UTC у локальний
+            addWeather4days(
+                this.weather[0].icon,
+                moment(localTime).calendar(),	// Використовуємо moment.js для представлення дати
+                this.weather[0].description,
                 Math.round(this.temp.day) + '&deg;C'
             );
         });
-        $('#location').html(city + ', <b>' + country + '</b>'); // Додаємо локацію на сторінку
+        
+        
+        $('#location').html(city + ', <b>' + country + '</b>' + days); // Додаємо локацію на сторінку
     }
     
-     function addWeather(icon, day, condition, temp){
+     function addWeather(icon, day, condition, temp, humidity, speed){
         var markup =  '<div class="day-today"><p><span>' + day +'</span></p></div>' +
             '<div class="icon-today"><p><span>' + '<img src="images/icons/'+ 
                   ( (icon === '10ddd')? '10d' : icon) // Fix in case if server returns unknown icon 10ddd 
                   + '.png" />' + '</span></p></div>' +
             '<div class="temp-today"><p><span>' + temp + '</span></p></div>' +
             '<div class="cond-today"><p><span>' + condition + '</span></span></p></div>' +
-            '<div class="wind-today"><p>Wind: <span>' + day + '</span></p></div>' +
-            '<div class="hum-today"><p>Humidity: <span>' + day + '</span></p></div>';
-        today.innerHTML = markup; // Додаємо рядок до таблиці
+            '<div class="wind-today"><p>Wind: <span>' + speed  + '</span></p></div>' +
+            '<div class="hum-today"><p>Humidity: <span>' + humidity + '</span></p></div>';
+        today.innerHTML = markup; 
     }
     
-/*
-    function addWeather(icon, day, condition, temp){
-        var markup = '<tr>'+
-                '<td>' + day + '</td>' +
-                '<td>' + '<img src="images/icons/'+ 
+
+  function addWeather4days(icon, day, condition, temp){
+        var markup = '<div class="day"> <p><span>' + day + '</span></p></div>' +
+                '<div class="temp"> <p><span>' + temp + '</span></p></div>' +
+                '<div class="icon"> <p><span>' + '<img src="images/icons/'+ 
                   ( (icon === '10ddd')? '10d' : icon) // Fix in case if server returns unknown icon 10ddd 
-                  +'.png" />' + '</td>' +
-                '<td>' + temp + '</td>' +
-                '<td>' + condition + '</td>'
-            + '</tr>';
-        weatherTable.insertRow(-1).innerHTML = markup; // Додаємо рядок до таблиці
+                  + '.png" />' + '</span></p></div>' +
+                '<div class="cond"> <p><span>' + condition + '</span></span></p></div>';
+         day1.innerHTML = markup; 
     }
-*/
+
+
     function showError(msg){
         $('#error').html('Сталася помилка: ' + msg);
     }
